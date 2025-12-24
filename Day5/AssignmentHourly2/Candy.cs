5ddd1a22117d93045f55e74a24e73ba3d7399a5c
Himanshu Panda <121447495+HimanshuPanda7777@users.noreply.github.com>
2025-12-24 09:29:00 +0530

    Practiced Assignments question -Preparation for M1

---
+++ Day5/AssignmentHourly2/Candy.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace AssignmentHourly2;
+
+public class Candy
+{
+    public string? Flavour{get; set;}
+    public int Quantity {get; set;}
+    public int PricePerPiece{get; set; }
+    public double TotalPrice{get; set;}
+    public double Discount{get; set; }
+    
+    public bool ValidateCandyFlavour(String Flavour)
+    {
+        if (Flavour == "Strawberry" || Flavour == "Lemon"|| Flavour == "Mint"  )
+        {
+            return true;
+            
+        }
+        else  
+        {
+            return false;
+            }
+    }
+
+}
---
+++ Day5/AssignmentHourly2/Program.cs
@@ -0,0 +1,55 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System.Numerics;
+using AssignmentHourly2;
+
+public class Program
+{
+    public Candy CalculateDiscountPrice(Candy c)
+    {
+        
+        c.TotalPrice=c.Quantity*c.PricePerPiece;
+        if(c.Flavour=="Strawberry"){
+        c.Discount=c.TotalPrice-(c.TotalPrice* 0.15);
+        }
+        else if(c.Flavour=="Lemon"){
+        c.Discount=c.TotalPrice-(c.TotalPrice* 0.10);
+        }
+        else if(c.Flavour=="Mint"){
+        c.Discount=c.TotalPrice-(c.TotalPrice* 0.05);
+        }
+        return c;
+
+
+    }
+    public static void Main()
+    {
+        Console.WriteLine("Enetr your Flavour");
+        string Flavour=Console.ReadLine()!;
+        Console.WriteLine("Enetr your Quantity");
+        int Quantity=Int32.Parse(Console.ReadLine()!);
+        Console.WriteLine("Enetr your price");
+        int PricePerPiece=Int32.Parse(Console.ReadLine()!);
+
+        Candy c=new Candy();
+        Program p=new Program();
+        if (c.ValidateCandyFlavour(Flavour))
+        {
+            p.CalculateDiscountPrice(c);
+            Console.WriteLine($"Flavour is {c.Flavour}");
+              Console.WriteLine($"Quantity is {c.Quantity}");
+                Console.WriteLine($"Price is {c.PricePerPiece}");
+                  Console.WriteLine($"Total Price is {c.TotalPrice}");
+                    Console.WriteLine($"Discount is {c.Discount}");
+
+        }
+    else {
+        Console.WriteLine("Yeh Flavour Nahi milege");
+    }
+         
+        
+
+
+    }
+}
