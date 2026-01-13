3f09ce888c18cfdac4017fa0fb6af43d5e6637a1
Himanshu Panda <121447495+HimanshuPanda7777@users.noreply.github.com>
2026-01-13 13:52:56 +0530

    LINQ Introduction

---
+++ Day24/LINQ_ConsoleAPP/LINQ_ConsoleAPP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LINQ_ConsoleAPP
+{
+    class Program
+    {
+        public static void LinqToObject()
+        {
+            int[] numArray = { 10, 2, 4, 8, 39, 33, 42, 84, 67, 90, 3, 22 };
+
+            string[] nameArray = { "Himanshu ", "Shreema", "leena", "purva", "Sheryl", "keerti", "Khushi", "Anindita", "Ocean" };
+            //foreach (int item in numArray)
+            //{
+            //    if (item % 2 == 0)
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+            //}
+            var result = from data in nameArray
+                         where data.Contains("a")
+                         select data;
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public static void Main(String[] args)
+        {
+            LinqToObject();
+        }
+    }
+}
+    
