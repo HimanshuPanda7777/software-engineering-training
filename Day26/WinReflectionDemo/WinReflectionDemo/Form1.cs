bd2132719873949ba590387ad19674ac5c665e0d
Himanshu Panda <121447495+HimanshuPanda7777@users.noreply.github.com>
2026-01-16 12:41:26 +0530

    Service

---
+++ Day26/WinReflectionDemo/WinReflectionDemo/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinReflectionDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string filePath = openFileDialog1.FileName;
+            Assembly assembly = Assembly.LoadFrom(filePath); 
+            
+            Type[] types = assembly.GetTypes();
+            p.ReflectAll(myType[0]);
+
+
+        }
+    }
+}
